27cd7684bc19edce2d14f68b1cc6876a96950d9c
gmmirror <blackstar76776@gmail.com>
2026-04-02 18:10:45 +0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+termux=false
+linux=false
+
+function for_termux() {
+    cd "${PREFIX}/opt/holehe"
+    command pip install --upgrade pip setuptools wheel
+    command pip install .
+    cd
+}
+
+function for_linux() {
+    cd "${PREFIX}/opt/holehe"
+    command python3 -m venv holehe_venv
+    source "holehe_venv/bin/activate"
+    command pip install --upgrade pip setuptools wheel
+    command pip install .
+    deactivate
+    cd
+
+    echo \
+        '#!/usr/bin/env bash' \
+        > "${PREFIX}/bin/holehe"
+
+    echo -e \
+        "exec ${PREFIX}/opt/holehe/holehe_venv/bin/python3 -m holehe \"\${@}\"" \
+        >> "${PREFIX}/bin/holehe"
+
+    command chmod +x "${PREFIX}/bin/holehe"
+}
+
+if [[ -n "${TERMUX_VERSION}" ]]; then
+    termux=true
+elif [[ -x "/system/bin/getprop" ]] || [[ -d "/data/data/com.termux" ]]; then
+    termux=true
+elif [[ -f "/etc/os-release" ]]; then
+    if ! command grep -iq "termux" "/etc/os-release"; then
+        linux=true
+    else
+        termux=true
+    fi
+fi
+
+if [[ ! -d "${PREFIX}/opt" ]]; then
+    command mkdir -p "${PREFIX}/opt"
+fi
+
+if [[ -d "${PREFIX}/opt/holehe" ]]; then
+    command rm -rf "${PREFIX}/opt/holehe"
+fi
+
+command git clone --depth 1 \
+    'https://github.com/megadose/holehe' \
+    "${PREFIX}/opt/holehe"
+
+if [[ "${termux}" == true ]]; then
+    for_termux
+elif [[ "${linux}" == true ]]; then
+    for_linux
+fi
